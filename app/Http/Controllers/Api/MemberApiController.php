f9343bd2b96af30502c903af2373e959b0713f79
febyataalya <alyadhini20@gmail.com>
2026-05-26 07:05:28 +0700

    Update

--- app/Http/Controllers/Api/MemberApiController.php
+++ app/Http/Controllers/Api/MemberApiController.php
@@ -63,7 +63,6 @@ class MemberApiController extends Controller
 
     public function getLokasis()
     {
-    {
     // =========================
     // AMBIL SEMUA LOKASI
     // =========================
